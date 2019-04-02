865e81f1aed672f2db4c3e3c88dbacc042993832
Emery Porter <emeryporter99@gmail.com>
2019-04-02 09:15:34 -0500

    * Changed "writeObject" to "q_writeObject" + Added "writeObject" method * Changed "filePath" parameter to "fileName"

--- src/FileStuff/Serializer.java
+++ src/FileStuff/Serializer.java
@@ -7,11 +7,11 @@ public class Serializer {
     /**
      * Serializes an object to a file.
      * @param obj the object to serialize
-     * @param filePath the file to serialize into
+     * @param fileName the file to serialize into
      */
-    public static void writeObject(Object obj, String filePath) {
+    public static void writeObject(Object obj, String fileName) {
         try{
-            FileOutputStream fileOut = new FileOutputStream(filePath);
+            FileOutputStream fileOut = new FileOutputStream(fileName);
             ObjectOutputStream outputStream = new ObjectOutputStream(fileOut);
             outputStream.writeObject(obj);
             outputStream.close();
@@ -20,17 +20,16 @@ public class Serializer {
         }
     }
 
-
     /**
-     * Deserializes an object from a file.
-     * @param filePath the file to read from
-     * @return the deserialized object
+     * Deserializes an object from a file. Generically handles all exceptions.
+     * @param fileName the file to read from
+     * @return the deserialized object or null if an error occurred
      */
     @SuppressWarnings("unchecked")
-    public static <T> T readObject(String filePath){
+    public static <T> T q_ReadObject(String fileName){
         T obj = null;
         try{
-            FileInputStream fileIn = new FileInputStream(filePath);
+            FileInputStream fileIn = new FileInputStream(fileName);
             ObjectInputStream in = new ObjectInputStream(fileIn);
             obj = (T) in.readObject();
             in.close();
@@ -42,8 +41,26 @@ public class Serializer {
         } catch (Exception e){
             System.out.println("An unknown error has occurred.");
         }
+        if(obj == null){
+            System.out.println("Warning, the deserialized object is null.");
+        }
         return obj;
     }
 
-
+    /**
+     * Deserializes an object from a file.
+     * @param fileName the file to read from
+     * @return the deserialized object
+     * @throws IOException if there is a problem opening the file
+     * @throws ClassNotFoundException if the data type specified doesn't exist
+     */
+    public static <T> T readObject(String fileName) throws IOException, ClassNotFoundException{
+        T obj;
+        FileInputStream fileIn = new FileInputStream(fileName);
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        obj = (T) in.readObject();
+        in.close();
+        fileIn.close();
+        return obj;
+    }
 }
