3fa9113c959af3c3b75317ef7457f026659f3817
Bluepuff71 <emeryporter99@gmail.com>
2019-04-11 11:19:41 -0500

    Added some debug startup logging

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -15,32 +15,46 @@ public class Runner {
     public static void main(String[] args) {
         InventoryManager inventoryManager;
         LoginManager loginManager;
+
         try {
+            System.out.println("[INFO]: Getting inventory file");
             inventoryManager = Serializer.readObject("Inventory.dat");
+            System.out.println("[INFO]: Inventory successfully loaded");
         } catch (IOException e) {
+            System.out.println("[ERROR]: No Inventory file found");
+            System.out.println("[INFO]: Creating new inventory manager");
             inventoryManager = new InventoryManager();
+            System.out.println("[INFO]: Inventory manager created successfully");
+            System.out.println("[INFO]: Filling inventory with default items");
+            inventoryManager.createInventoryOrder(new Item("Black Shirt", 30), 12);
+            inventoryManager.createInventoryOrder(new Item("Cool Socks", 6.36), 17);
+            inventoryManager.createInventoryOrder(new Item("Extraordinarily Large Drum", 830), 1);
+            inventoryManager.createInventoryOrder(new Item("Broken Chair", 15.67), 2);
+            inventoryManager.createInventoryOrder(new Item("Extremely Broken Chair", 8.54), 1);
+            inventoryManager.createInventoryOrder(new Item("Pile of saw dust", .54), 400);
+            inventoryManager.createInventoryOrder(new Item("Quantum Entanglement Device", 860000), 15);
+            inventoryManager.createInventoryOrder(new Item("sICK Skateboard", 24), 32);
+            inventoryManager.createInventoryOrder(new Item("Interesting goo", 12.25), 46);
+            inventoryManager.createInventoryOrder(new Item("Bag of lukewarm milk", 3.38), 5);
+            System.out.println("[INFO]: Items added successfully");
         } catch (ClassNotFoundException e) {
             System.out.println("No class found. This is a problem.");
             return;
         }
         try {
+            System.out.println("[INFO]: Getting accounts file");
             loginManager = Serializer.readObject("Accounts.dat");
+            System.out.println("[INFO]: Accounts successfully loaded");
         } catch (IOException e) {
+            System.out.println("[ERROR]: No Accounts file found");
+            System.out.println("[INFO]: Creating new login manager");
             loginManager = new LoginManager();
+            System.out.println("[INFO]: Login manager created successfully");
         } catch (ClassNotFoundException e) {
             System.out.println("No class found. This is a problem.");
             return;
         }
-        /*inventoryManager.createInventoryOrder(new Item("Black Shirt", 30), 12);
-        inventoryManager.createInventoryOrder(new Item("Cool Socks", 6.36), 17);
-        inventoryManager.createInventoryOrder(new Item("Extraordinarily Large Drum", 830), 1);
-        inventoryManager.createInventoryOrder(new Item("Broken Chair", 15.67), 2);
-        inventoryManager.createInventoryOrder(new Item("Extremely Broken Chair", 8.54), 1);
-        inventoryManager.createInventoryOrder(new Item("Pile of saw dust", .54), 400);
-        inventoryManager.createInventoryOrder(new Item("Quantum Entanglement Device", 860000), 15);
-        inventoryManager.createInventoryOrder(new Item("sICK Skateboard", 24), 32);
-        inventoryManager.createInventoryOrder(new Item("Interesting goo", 12.25), 46);
-        inventoryManager.createInventoryOrder(new Item("Bag of lukewarm milk", 3.38), 5);*/
+
         while (true) {
             System.out.println("Which user are you?");
             System.out.println("[1] Customer");
